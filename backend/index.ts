c9e2aa23b20eeb0630c939d18799f5bbf065d06f
Heet Sheth <97078945+Heet-Sheth@users.noreply.github.com>
2026-01-29 22:44:13 +0530

    install cors and allow FE connect FE with BE

--- backend/index.ts
+++ backend/index.ts
@@ -1,10 +1,13 @@
-import express from 'express';
+import express from "express";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!?#!");
 });
 
 app.listen(PORT, () => {
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
+
 function App() {
+  const [basicString, useBasicString] = useState(String);
+  fetch("http://localhost:3000")
+    .then((res) => res.text())
+    .then((data) => useBasicString(data));
   return (
-    <div>  
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1></div>
-  )
+    <div>
+      <h1 className="text-3xl font-bold underline">{basicString}</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
