fc37b193479865bbb0f201f7b3eccdbe0e77e1ff
Heet Sheth <97078945+Heet-Sheth@users.noreply.github.com>
2026-01-29 22:48:18 +0530

    install nodemon to live sync changes to backend.

--- backend/index.ts
+++ backend/index.ts
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Hello, World!?#!");
+  res.send("Hello, World!!!");
 });
 
 app.listen(PORT, () => {
