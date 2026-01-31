6b1113312c622218771fa33bfb90fca685da46e2
externalPointerVariable <vicky986847@gmail.com>
2026-01-31 10:16:10 +0000

    Created Utility function for excel parsing

---
+++ backend/utils/sheet.util.ts
@@ -0,0 +1,13 @@
+import * as XLSX from 'xlsx';
+
+export class SheetUtil {
+  static jsonToSheet(data: any[], sheetName: string = 'Sheet1'): XLSX.WorkSheet {
+    const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(data);
+    return worksheet;
+  }
+
+  static sheetToJson(worksheet: XLSX.WorkSheet): any[] {
+    const data: any[] = XLSX.utils.sheet_to_json(worksheet);
+    return data;
+  }
+}
