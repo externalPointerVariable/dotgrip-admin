93793d395b962609576538ddb422ff3f32bbf780
externalPointerVariable <vicky986847@gmail.com>
2026-03-12 06:40:26 +0000

    Unstaged Changes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,18 +1,8 @@
 import { Provider } from "@/components/ui/provider";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import LoginPage from "./pages/Login";
-
-let content;
-let isAuthenticated = localStorage.getItem("authToken") !== null; 
-if (isAuthenticated) {
-  content = <App />;
-} else {
-  content = <LoginPage />;
-}
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <Provider>
-      {content}
+      <App />
     </Provider>
 );
