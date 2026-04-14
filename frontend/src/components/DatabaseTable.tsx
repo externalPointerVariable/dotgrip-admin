3b3bb2db326cdbf714248128454d249e4013f360
externalPointerVariable <vicky986847@gmail.com>
2026-04-14 10:40:13 +0000

    Made some frontend changes

--- frontend/src/components/DatabaseTable.tsx
+++ frontend/src/components/DatabaseTable.tsx
@@ -12,7 +12,7 @@ import { useState } from "react";
 import { LuExternalLink } from "react-icons/lu";
 
 interface DatabaseTableProps {
-  influencers: [Influencer] | null;
+  influencers: Influencer[] | null;
 }
 
 export default function DatabaseTable({ influencers }: DatabaseTableProps) {
@@ -45,47 +45,60 @@ export default function DatabaseTable({ influencers }: DatabaseTableProps) {
             <Table.ColumnHeader>Status</Table.ColumnHeader>
           </Table.Row>
         </Table.Header>
+
         <Table.Body>
-          {influencers &&
-            influencers.length > 0 &&
-            influencers.map((influencer) => {
-              return (
-                <Table.Row key={influencer._id}>
-                  <Table.Cell>
-                    <Box>
-                      <Text onClick={() => setCurrent(influencer)}>
-                        {influencer.name}
-                      </Text>
-                      <br />
-                      <HStack>
-                        {"@" + influencer.instagramLink.split("/")[3]}
-                        <a href={influencer.instagramLink} target="_blank">
-                          <LuExternalLink />
-                        </a>
-                      </HStack>
-                    </Box>
-                  </Table.Cell>
-                  <Table.Cell>{influencer.instagram?.followerCount}</Table.Cell>
-                  <Table.Cell>
-                    <RatingGroup.Root
-                      colorPalette={"yellow"}
-                      allowHalf
-                      readOnly
-                      count={5}
-                      value={influencer?.contentRating || 0}
-                    >
-                      <RatingGroup.HiddenInput />
-                      <RatingGroup.Control />
-                    </RatingGroup.Root>
-                  </Table.Cell>
-                  <Table.Cell>{influencer.primeNiche}</Table.Cell>
-                  <Table.Cell>{influencer.instagram?.averageViews}</Table.Cell>
-                  <Table.Cell>{influencer.instagram?.averageLikes}</Table.Cell>
-                  <Table.Cell>{influencer.address?.[0].city}</Table.Cell>
-                  <Table.Cell>{influencer.taskStatus}</Table.Cell>
-                </Table.Row>
-              );
-            })}
+          {influencers
+            ?.filter((inf) => inf.taskStatus === "approved")
+            .map((influencer) => (
+              <Table.Row key={influencer._id}>
+                <Table.Cell>
+                  <Box>
+                    <Text onClick={() => setCurrent(influencer)}>
+                      {influencer.name}
+                    </Text>
+
+                    <HStack>
+                      {"@" +
+                        influencer.instagramLink
+                          .split("/")[3]
+                          ?.split("?")[0]}
+                      <a href={influencer.instagramLink} target="_blank">
+                        <LuExternalLink />
+                      </a>
+                    </HStack>
+                  </Box>
+                </Table.Cell>
+
+                <Table.Cell>
+                  {influencer.instagram?.followerCount}
+                </Table.Cell>
+
+                <Table.Cell>
+                  <RatingGroup.Root
+                    colorPalette="yellow"
+                    allowHalf
+                    readOnly
+                    count={5}
+                    value={influencer?.contentRating || 0}
+                  >
+                    <RatingGroup.HiddenInput />
+                    <RatingGroup.Control />
+                  </RatingGroup.Root>
+                </Table.Cell>
+
+                <Table.Cell>{influencer.primeNiche}</Table.Cell>
+                <Table.Cell>
+                  {influencer.instagram?.averageViews}
+                </Table.Cell>
+                <Table.Cell>
+                  {influencer.instagram?.averageLikes}
+                </Table.Cell>
+                <Table.Cell>
+                  {influencer.address?.[0]?.city}
+                </Table.Cell>
+                <Table.Cell>{influencer.taskStatus}</Table.Cell>
+              </Table.Row>
+            ))}
         </Table.Body>
       </Table.Root>
     </Center>
