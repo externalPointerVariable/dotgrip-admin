6a3744c1009c35410b836c718934d0bd8cff4821
externalPointerVariable <vicky986847@gmail.com>
2026-04-19 12:07:23 +0000

    type chages

--- backend/index.ts
+++ backend/index.ts
@@ -3,8 +3,9 @@ import cors from "cors";
 import connectDB from "./utils/mongo.utils.ts";
 import { AuthRoutes } from "./routes/auth.routes.ts";
 import influencerRoutes from "./routes/influencer.routes.ts";
+import {Application} from "express";
 
-const app = express();
+const app: Application = express();
 const PORT = process.env.PORT || 3000;
 
 
